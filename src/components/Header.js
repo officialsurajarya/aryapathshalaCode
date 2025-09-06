fc2362380ac3686f3d74c9e7a96a077232fb6512
officialsurajarya <officialsurajarya@gmail.com>
2025-09-06 14:39:39 +0530

    Nav Toogler

---
+++ src/components/Header.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { Container, Nav, Navbar, NavDropdown, } from 'react-bootstrap'
+import '../App.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faBook, faBullhorn, faEnvelope, faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+
+export default function Header() {
+    const [expanded, setExpanded] = useState(false);
+    return (
+        <Navbar expand="lg" className="p-0 navbar">
+            <Container className='NavContainer'>
+                <Navbar.Brand href="#" className='p-0'>
+                    <img
+                        src="./logo.png"
+                        width="80"
+                        alt="Arya Pathshala Logo"
+                    />
+                </Navbar.Brand>
+
+                <Navbar.Toggle
+                    aria-controls="my-nav"
+                    onClick={() => setExpanded(expanded ? false : "expanded")}
+                >
+                    {expanded ? (
+                        <FontAwesomeIcon icon={faTimes} size="lg" />
+                    ) : (
+                        <FontAwesomeIcon icon={faBars} size="lg" />
+                    )}
+                </Navbar.Toggle>
+
+                <Navbar.Collapse id="my-nav">
+                    <Nav className="m-auto fw-bold text-center gap-3">
+                        {/* Home */}
+                        <Nav.Link href="#home" active>
+                            <FontAwesomeIcon icon={faHome} className="navIcon" /> मुखपृष्ठ
+                        </Nav.Link>
+
+                        {/* Dropdown: पाठ्यक्रम */}
+                        <NavDropdown title={<><FontAwesomeIcon icon={faBook} className="navIcon" /> पाठ्यक्रम</>} id="coursesDropdown">
+                            <NavDropdown.Item href="#ved">वेद</NavDropdown.Item>
+                            <NavDropdown.Item href="#vedang">वेदांग</NavDropdown.Item>
+                            <NavDropdown.Item href="#upanishad">उपनिषद्</NavDropdown.Item>
+                            <NavDropdown.Item href="#darshan">दर्शनशास्त्र</NavDropdown.Item>
+                            <NavDropdown.Item href="#aarshe">अन्य आर्षग्रन्थ</NavDropdown.Item>
+                            <NavDropdown.Item href="#netexam1">राष्ट्रीय पात्रता परीक्षा (संस्कृत 25)</NavDropdown.Item>
+                            <NavDropdown.Item href="#netexam2">राष्ट्रीय पात्रता परीक्षा (संस्कृत 73)</NavDropdown.Item>
+                        </NavDropdown>
+
+                        {/* Dropdown: पुस्तकालय */}
+                        <NavDropdown title={<><FontAwesomeIcon icon={faBook} className="navIcon" /> पुस्तकालय</>} id="libraryDropdown">
+                            <NavDropdown.Item href="#mainBook">मूल ग्रन्थ</NavDropdown.Item>
+                            <NavDropdown.Item href="#VaykhyanBook">व्याख्यान ग्रन्थ</NavDropdown.Item>
+                            <NavDropdown.Item href="#VidhwatLekh">विद्वत्-लेख एवं शोध पत्र</NavDropdown.Item>
+                        </NavDropdown>
+
+                        {/* Notices */}
+                        <Nav.Link href="#notices">
+                            <FontAwesomeIcon icon={faBullhorn} className="navIcon" /> सूचनाएँ
+                        </Nav.Link>
+
+                        {/* Contact */}
+                        <Nav.Link href="#contact">
+                            <FontAwesomeIcon icon={faEnvelope} className="navIcon" /> शंका समाधान एवं सम्पर्क
+                        </Nav.Link>
+
+                    </Nav>
+
+
+                </Navbar.Collapse>
+            </Container >
+        </Navbar >
+    )
+}
